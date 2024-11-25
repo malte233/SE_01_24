af51b4133908d6b69d9af0c6dcf598d69c52c54b
malte233 <malte.werner@code.berlin>
2024-11-25 14:37:23 +0100

    reformatting

--- Assesment01.py
+++ Assesment01.py
@@ -1,7 +1,11 @@
 data = [[1, 0, 0], [2, 1, 2], [2, 1, 0]]
+
 print("1: " + str(data[0]))
 print("2: " + str(data[1]))
 print("3: " + str(data[2]))
+
 print("What is your move? Example move: '1 2' -> liquid from one poured into 2")
+
 move=input()
+
 print(move)
